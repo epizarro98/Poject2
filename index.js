4ea56b5134ed0a3ad82bd54c4210b95682f83bbc
epizarro98 <epizarro309@gmail.com>
2020-11-10 14:24:00 -0500

    cloudinary is now working

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let cloudinary = require('cloudinary');
 let multer = require('multer');
 const db = require('./models/index.js');
 let upload = multer({ dest: './uploads/' });
-let imgUrl = cloudinary.url('i5gehdy7vlsmx20ug32x', {width: 250, height: 250})
+let imgUrl = cloudinary.url('ivxhhdczxofx3rtze0cg', {width: 250, height: 250})
 
 
 //setup ejs and ejs layouts
@@ -18,7 +18,6 @@ cloudinary.config(process.env.CLOUDINARY_URL)
 
 
 app.get('/', function(req, res) {
-    res.render('home');
     res.render('home', { image: imgUrl });
   });
 
@@ -27,6 +26,7 @@ app.get('/', function(req, res) {
 app.post('/', upload.single('myFile'), function(req, res) {
   cloudinary.uploader.upload(req.file.path, function(result) {
     // res.send(result.url);
+    imgUrl = result.url
     db.mail.findOrCreate({
         where: {
             url: result.url
@@ -35,6 +35,7 @@ app.post('/', upload.single('myFile'), function(req, res) {
         db.user.findOne()
         .then(user=>{
             user.addMail(mail)
+            res.redirect('/')
             })
         .catch(err=>{
             console.log(err, 'ERROR <=====================')
@@ -51,11 +52,48 @@ app.get('/', (req, res)=>{
 })
 
 // THIS ROUTE WILL BE FOR CATEGORIZED MAIL ONCE WE GET CLOUDINARY TO STOP PLAYIN
+app.get('/', (req, res)=>{
+    // res.send('EXPRESS AUTH HOME ROUTE')
+    res.render('category')
+})
+
+//============================
+
+
 // app.get('/', (req, res)=>{
-//     // res.send('EXPRESS AUTH HOME ROUTE')
-//     res.render('category')
+//     db.category.findAll()
+//     .then((categories)=>{
+//         //render to category index, pass in category in the route
+//         res.render('categories/index', {categories: categories})
+//     })
+//     .catch(err =>{
+//         console.log(err)
+//     })
 // })
 
+app.get('/:id', (req, res)=>{
+    // res.send('hellooooooooo')
+    console.log(req.params.id)
+    db.category.findOne({
+        where: {id: req.params.id},
+        include: [db.mail]
+    })
+    .then((category)=>{
+        console.log(category)
+        //render category show page, pass in category to res.render in the show page
+        if (!category) throw Error()
+        res.render('category', {category: category, user: category.user})
+        // res.send('hello')
+    })
+    .catch(err =>{
+        console.log('THIS IS THE ERROR<================', err)
+    })
+})
+
+
+
+
+//============================
 app.listen(3000, ()=>{
     console.log('youre now in port 3000')
 })
