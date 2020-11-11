2316f6d7c55ad07b6eac2f2bfbe8ca587026168a
epizarro98 <epizarro309@gmail.com>
2020-11-10 22:26:22 -0500

    reset auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,51 +1,55 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../models')
+const passport = require('../config/ppConfig.js')
 
-router.get('/signup',(req, res)=>{
+router.get('/signup', (req, res)=>{
     res.render('auth/signup')
+})
+
+router.post('/signup', (req, res)=>{
+    console.log('sign up form user input:', req.body)
+    // if it does, throw an error message
+    // otherwise create a new user and store them in the db
+    db.user.findOrCreate({ // check if that email is already in db
+        where: {email: req.body.email},
+        defaults: {name: req.body.name, password: req.body.password}
+    }) // create new user if email wasn't found
+    .then(([createdUser, wasCreated])=>{
+        if(wasCreated){
+            console.log(`just created the following user:`, createdUser)
+            // log the new user in
+            passport.authenticate('local', {
+                successRedirect: '/',
+                successFlash: 'Account created and logged in!'  // !-> FLASH <-!
+            })(req, res) // IIFE = immediately invoked function
+        } else {
+            req.flash('error', 'email already exists, try logging in')  // !-> FLASH <-!
+            res.redirect('/auth/login') // redirect to login page
+            // console.log('An account associated with that email address already exists! Try loggin in.')
+        }
+    })
+    .catch(err=>{
+        req.flash('error', err.message)  // !-> FLASH <-!
+        res.redirect('/auth/signup') // redirect to signup page so they can try again
+    })
 })
 
 router.get('/login', (req, res)=>{
     res.render('auth/login')
 })
 
-router.get('/signup', (req, res)=>{
-    res.send('GET /auth signup successfully')
-})
+router.post('/login', passport.authenticate('local', {
+    failureRedirect: '/auth/login',
+    successRedirect: '/',
+    failureFlash: 'Invalid email or password!', // !-> FLASH <-!
+    successFlash: 'You are now logged in!' // !-> FLASH <-!
+}))
 
-router.post('/signup', (req, res)=>{
-    console.log('sign up form user', req.body)
-
-    //check if user aleadt exists
-    //if it does, throw an error
-    //otherwise create a new user and store them in the db
-    db.user.findOrCreate({
-        where: {email: req.body.email},
-        defaults:{
-            name: req.body.name,
-            password: req.body.password
-        }
-    }) // created new user if email wasnt found
-    .then(([createdUser, wasCreated])=>{
-        if(wasCreated){
-            console.log('just created the following user:', createdUser)
-        }else{
-            console.log('already exist, try again')
-        }
-    })
-    res.redirect('/auth/login')
-})
-
-router.get('/login', (req, res)=>{
-    res.send('GET /auth/login successfully hit')
-})
-
-router.post('/login', (req, res)=>{
-    console.log('Trying to log in', req.body)
-    //redirect to home route
+router.get('/logout', (req, res)=>{
+    req.logout()
+    req.flash('Successfully logged out!') // !-> FLASH <-!
     res.redirect('/')
 })
-
 
 module.exports = router
